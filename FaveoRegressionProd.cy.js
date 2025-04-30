311eca8b1dde85d025ddabd39e1ece2bfa41a232
QA-Arjie <arjie.viguilla@gmail.com>
2025-04-30 10:59:20 +0800

    Add files via upload

---
+++ FaveoRegressionProd.cy.js
@@ -0,0 +1,258 @@
+
+// Run the Login script first
+
+describe('Faveo Prod Test', () => {
+    const timestamp = new Date().toISOString().replace(/[^0-9]/g, '').slice(0, 14);
+    const ticketSubject = `Test Ticket Only-${timestamp}`;
+    let createdTicketSubject;
+    const testResults = [];
+    let testStartTime;
+  
+    // Handle uncaught exceptions globally (for the entire test suite)
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // Returning false prevents Cypress from failing the test
+        return false;
+    });
+  
+    beforeEach(() => {
+        // Read cookies from the file and set them
+        cy.readFile('fixtures/cookies.json').then((cookies) => {
+            cookies.forEach((cookie) => {
+                cy.setCookie(cookie.name, cookie.value, {
+                    domain: cookie.domain,
+                    path: cookie.path,
+                    secure: cookie.secure,
+                    expiry: cookie.expiry,
+                });
+            });
+        });
+  
+        // Optionally reload the page after setting the cookies to apply them
+        cy.reload(); 
+  
+        // Record start time before each test
+        testStartTime = Date.now();
+  
+    });
+  
+    afterEach(function() {
+      // Calculate test duration in seconds
+      const duration = ((Date.now() - testStartTime) / 1000).toFixed(2);
+  
+      // Access test properties directly from mocha's context Update
+      testResults.push({
+        testName: this.currentTest.title,
+        suite: 'Faveotest',
+        status: this.currentTest.state || 'unknown',
+        errorMessage: this.currentTest.err ? this.currentTest.err.message : '',
+        timestamp: new Date().toISOString(),
+        duration: duration
+      });
+  
+      // Take screenshot
+      cy.screenshot(`${timestamp}/${this.currentTest.title}`);
+    });
+  
+    // Test case 1: Create new Ticket
+    it('TC01: Create Ticket', () => {
+        cy.visit('https://ticketing.myrepublic.com.sg/panel/newticket')
+          cy.get('.card-title').should('be.visible')
+          // Form
+          cy.get('#vs1__combobox > .vs__selected-options > #dynamic-select').type('Test Form for Auto');
+          cy.wait(2000)
+          cy.get('#vs1__option-0 > .d-center').click();
+          cy.get('#Requester > .col-md-2 > .v-popper--has-tooltip').should('be.visible')
+          cy.wait(1000);
+          // Requester
+          cy.get('#requester').type('Arjie Test').should('have.value', 'Arjie Test')
+          cy.wait(1000)
+          cy.get('#vs13__option-0 > .d-center').click();
+  
+          //Subject
+          cy.get("#subject").type(ticketSubject);
+          // Description
+          cy.get('iframe').then(($iframe) => {
+              const body = $iframe.contents().find('body'); 
+              cy.wrap(body).type('Test ticket only, please ignore');
+            });
+        
+          // Assigned
+          cy.get('#assigned_id').type('Arjie Viguilla').should('have.value', 'Arjie Viguilla')
+          cy.get('#vs14__option-0 > .d-center').click();
+          // Status
+          cy.get('#status_id').click();
+          cy.get('#vs15__option-0 > .d-center').click();
+          // Type
+          cy.get('#type_id').click();
+          cy.get('#vs16__option-2 > .d-center').click();
+          // Priority
+          cy.get('#priority_id').click();
+          cy.get('#vs17__option-1 > .d-center').click();
+          // Department
+          cy.get('#department_id').click();
+          cy.get('#vs18__option-3 > .d-center').click();
+          // Submit ticket //
+          cy.get('#form-submit > .fa').click()
+          // Assert // Verify ticket is created
+          //cy.get('#alert-message > div > span').should('be.visible')
+          cy.get('[style="border-top: 3px solid rgb(0, 166, 90);"] > :nth-child(1)').should('be.visible')
+          
+          // Store the ticket subject for later use in other Test Cases
+          createdTicketSubject = ticketSubject;
+    });
+
+        // Test Case 2: Search and Reply to Created ticket
+    it('TC02: Reply to Ticket', () => {
+        cy.visit('https://ticketing.myrepublic.com.sg/panel/dashboard')
+        cy.get('div > .fas').click()
+        cy.get('.input-group > .form-control').clear()
+        cy.get('.input-group > .form-control').type(`${createdTicketSubject}{enter}`).should('have.value', createdTicketSubject); // Assertion to verify the input value
+        cy.wait(1000)
+        cy.get('.input-group-append > :nth-child(1) > .fas').click()
+        //Assert
+        cy.get('div.dropdown-item').should('be.visible')
+        // Click ticket result
+        cy.get('#first_thread_title > :nth-child(2)').click()
+        // Assertion, verify if requester and assignee is correct.
+        cy.get(':nth-child(7) > .row > .inbox-info > .inline-block > :nth-child(1) > .user_name > .emphasize')
+        .contains('Arjie Test')
+        .should('be.visible')
+        cy.get(':nth-child(9) > .row > .inbox-info > .inline-block > :nth-child(1) > .user_name > .emphasize')
+        .contains('arjie viguilla')
+        .should('be.visible')
+        // Click Reply button
+        cy.get('.timeline-actions-span > :nth-child(5)').click()
+        cy.wait(2000);
+        // Type in the iframe
+        cy.get('#focus_editor_ifr').then($iframe => {
+        const body = $iframe.contents().find('body');
+        cy.wrap(body).type('Test reply');
+        });
+        // Submit Reply
+        cy.get('.card-footer > .btn').click()
+        // Assert
+        cy.get('#alert-message > div > span').should('be.visible')
+
+    })
+
+    // Test Case 3: Add Internal Notes
+    it('TC03: Add Internal Notes', () => {
+        cy.visit('https://ticketing.myrepublic.com.sg/panel/dashboard')
+        cy.get('div > .fas').click()
+        cy.get('.input-group > .form-control').clear()
+        cy.get('.input-group > .form-control').type(`${createdTicketSubject}{enter}`).should('have.value', createdTicketSubject); // Assertion to verify the input value
+        cy.wait(1000)
+        cy.get('.input-group-append > :nth-child(1) > .fas').click()
+        //Assert
+        cy.get('div.dropdown-item').should('be.visible')
+        // Click ticket result
+        cy.get('#first_thread_title > :nth-child(2)').click()
+        // Assertion, verify if requester and assignee is correct.
+        cy.get(':nth-child(7) > .row > .inbox-info > .inline-block > :nth-child(1) > .user_name > .emphasize')
+        .contains('Arjie Test')
+        .should('be.visible')
+        cy.get(':nth-child(9) > .row > .inbox-info > .inline-block > :nth-child(1) > .user_name > .emphasize')
+        .contains('arjie viguilla')
+        .should('be.visible')
+        // Click Add Note button
+        cy.get('.timeline-actions-span > :nth-child(6)').click()
+        cy.wait(2000);
+        // Type in the iframe
+        cy.get('#focus_editor_ifr').then($iframe => {
+        const body = $iframe.contents().find('body');
+        cy.wrap(body).type('Test Internal Note');
+        });
+        // Submit Reply
+        cy.get('.card-footer > .btn').click()
+        // Assert
+        cy.get('#alert-message > div > span').should('be.visible')
+    })
+  
+        // Test Case 4: Resolved Ticket
+    it('TC04: Resolved Ticket', () => {
+        cy.visit('https://ticketing.myrepublic.com.sg/panel/dashboard')
+        cy.get('div > .fas').click()
+        cy.get('.input-group > .form-control').clear()
+        cy.get('.input-group > .form-control').type(`${createdTicketSubject}{enter}`).should('have.value', createdTicketSubject); // Assertion to verify the input value
+        cy.wait(1000)
+        cy.get('.input-group-append > :nth-child(1) > .fas').click()
+        //Assert
+        cy.get('div.dropdown-item').should('be.visible')
+        // Click ticket result
+        cy.get('#first_thread_title > :nth-child(2)').click()
+        // Assertion, verify if requester and assignee is correct.
+        cy.get(':nth-child(7) > .row > .inbox-info > .inline-block > :nth-child(1) > .user_name > .emphasize')
+        .contains('Arjie Test')
+        .should('be.visible')
+        cy.get(':nth-child(9) > .row > .inbox-info > .inline-block > :nth-child(1) > .user_name > .emphasize')
+        .contains('arjie viguilla')
+        .should('be.visible')
+        // Click Status
+        cy.get('#status').click()
+        // Click Resolved button
+        cy.get('.card-tools > .btn-group > .dropdown-menu > :nth-child(4)').should('be.visible')
+        cy.get('.card-tools > .btn-group > .dropdown-menu > :nth-child(4)').click()
+        cy.wait(2000)
+        // Leave a Comment
+        cy.get('iframe').then(($iframe) => {
+            const body = $iframe.contents().find('body'); 
+            cy.wrap(body).type('Test ticket only');
+          });
+        // Click Proceed
+        cy.get('#submit_btn').click()
+        // Assert
+        cy.get('#alert-message > div > span').should('be.visible')
+    })
+
+    // Test Case 5: Fork Ticket
+    it('TC05: Fork Ticket', () => {
+        cy.visit('https://ticketing.myrepublic.com.sg/panel/dashboard')
+        cy.get('div > .fas').click()
+        cy.get('.input-group > .form-control').clear()
+        cy.get('.input-group > .form-control').type(`${createdTicketSubject}{enter}`).should('have.value', createdTicketSubject); // Assertion to verify the input value
+        cy.wait(1000)
+        cy.get('.input-group-append > :nth-child(1) > .fas').click()
+        //Assert
+        cy.get('div.dropdown-item').should('be.visible')
+        // Click ticket result
+        cy.get('#first_thread_title > :nth-child(2)').click()
+        // Assertion, verify if requester and assignee is correct.
+        cy.get(':nth-child(7) > .row > .inbox-info > .inline-block > :nth-child(1) > .user_name > .emphasize')
+        .contains('Arjie Test')
+        .should('be.visible')
+        cy.get(':nth-child(9) > .row > .inbox-info > .inline-block > :nth-child(1) > .user_name > .emphasize')
+        .contains('arjie viguilla')
+        .should('be.visible')
+        // Click More Button
+        cy.get('#more-evt > .btn > .fas').click()
+        // Assertion
+        cy.get('#more-evt > .dropdown-menu > :nth-child(3)').should('be.visible')
+        // Click Fork
+        cy.get('#more-evt > .dropdown-menu > :nth-child(3)').click()
+        // Assertion
+        cy.get('#Subject > :nth-child(1) > .v-popper--has-tooltip').should('be.visible')
+        // Edit Subject
+        cy.get('#subject').type("- Fork Ticket Test")
+        // Radio Button
+        cy.get(':nth-child(3) > div > [style="display: inline;"] > :nth-child(2) > .radio_align').click()
+        // Click Fork Button
+        cy.get('.modal-footer > .btn-primary').click()
+        cy.wait(2000)
+        // Assertion
+        cy.get('#alert-message > div > span').should('be.visible')
+    });
+
+    after(() => {
+      const csvContent = 'Test Name,Suite,Status,Error Message,Timestamp,Duration (seconds)\n' + 
+        testResults.map(result => 
+          `${result.testName},${result.suite},${result.status},${result.errorMessage || ''},${result.timestamp},${result.duration}`
+        ).join('\n');
+  
+      cy.task('writeCSV', {
+        filePath: `cypress/results/test-results-${timestamp}.csv`,
+        csvContent: csvContent
+      });
+    });
+  
+  });
+  
---
+++ Prod Login.cy.js
@@ -0,0 +1,26 @@
+describe('Faveotest', () => {
+    const timestamp = new Date().toISOString().replace(/[^0-9]/g, '').slice(0, 14);
+    const ticketSubject = `Test Ticket Only-${timestamp}`;
+    let createdTicketSubject;
+    const testResults = [];
+    let testStartTime;
+
+
+    // Test Case #1: Login
+  it('TC01 Login', () => {
+    cy.visit("https://ticketing.myrepublic.com.sg/knowledgebase");
+    cy.get("#client_login").click();
+    cy.get("#login_form_user_name").type(Cypress.env("user_name"));
+    cy.get("#login_form_password").type(Cypress.env("password"), { log: false });
+    cy.get("#default-login-button").click();
+    //cy.wait(2000);
+    //cy.get('.text').should('contain', 'You are here :');
+    cy.get('#navbarDropdownProfile').should('be.visible')
+
+    cy.getCookies().then((cookies) => {
+        const cookiesJSON = JSON.stringify(cookies);
+        cy.writeFile('fixtures/cookies.json', cookiesJSON);
+      });
+  });
+
+});
